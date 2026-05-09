6c65bffc33d75a78008d07be43414ea0b89432d8
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-09 15:10:59 +0330

    Coded week_02.cs

--- week_02.cs
+++ week_02.cs
@@ -1,1 +1,8 @@
-
+class week_02
+  static void Main()
+{
+Console.WriteLine("Hi!Im M A T! \n Please write something : );
+string something = Console.ReadLine();
+Console.Write("You said : ");
+Console.WriteLine(something);
+}
