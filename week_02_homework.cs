274689457d8f6cfedbee9eca40979a9c5e916ab5
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-11 15:54:55 +0330

    Update week_02_homework.cs
    A simple calculator

--- week_02_homework.cs
+++ week_02_homework.cs
@@ -1,1 +1,25 @@
+using System;
+class week_02_homework
+{
+    static void Main()
+    {
+       Console.Write("enter 1st number : ");
+       int a = int.Parse(Console.ReadLine());
+       Console.Write("enter 2nd number : ");
+       int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Sum (+) : " + (a+b));
+        Console.WriteLine("Dif (-) : " + (a-b));
+        Console.WriteLine("Product (x) : " + (a*b));
 
+       if(b == 0) {
+        Console.WriteLine("Div (/) : ERROR! ");
+        Console.WriteLine("Rem (%) : ERROR!");
+       }
+       else
+       {
+        Console.WriteLine("Div (/) : " + (a/b));
+        Console.WriteLine("Rem (%) : " + (a%b));
+       }
+
+    }
+}
